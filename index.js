d4643cfdf30b57f213d32fe2c5c206f62e92545d
Filip Chalupa <chalupa.filip@gmail.com>
2018-03-31 16:14:06 +0200

    Dummy game loop

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+
+const WebSocket = require('ws')
+const gameLoop = require('node-gameloop')
+
 console.log('Start')
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+const FIELD_SIZE = {
+	width: 50,
+	height: 50,
+}
+
+let lastClientId = 0
+
+wss.on('connection', (ws) => {
+	const id = ++lastClientId
+
+	const log = (text) => {
+		console.log(`[${id}] ${text}`)
+	}
+
+	ws.on('message', (message) => {
+		log(message)
+	})
+
+	ws.send('Hello')
+})
+
+const gameLoopId = gameLoop.setGameLoop((delta) => {
+	console.log('tick')
+}, /* 1000 / 30 */1000)
+
+
+process.on('SIGINT', () => {
+	console.log('Bye bye')
+	gameLoop.clearGameLoop(gameLoopId)
+	wss.close()
+	process.exit()
+})
