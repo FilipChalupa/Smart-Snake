30646a262a869e7eeb38b47c54ee1c16d27af563
Filip Chalupa <chalupa.filip@gmail.com>
2018-05-13 21:08:41 +0200

    Jarjar AI

--- another2/src/components/JarjarAI.ts
+++ another2/src/components/JarjarAI.ts
@@ -3,7 +3,7 @@ import { BoardFieldContent } from './Board'
 import Snake from './Snake'
 import { positionDelta, turnLeft, turnRight } from '../constants/Directions'
 
-export default class DummyAI {
+export default class JarjarAI {
 	private boardWidth: number
 	private boardHeight: number
 	private snake: Snake
@@ -36,29 +36,101 @@ export default class DummyAI {
 
 		const direction = this.snake.getDirection()
 		let delta = positionDelta(direction)
-		if (
-			!this.getContent(position.x + delta.x, position.y + delta.y).isObstacle()
-		) {
+
+		let forwardPositionX = position.x + delta.x
+		let forwardPositionY = position.y + delta.y
+
+		if (!this.getContent(forwardPositionX, forwardPositionY).isObstacle()) {
 			canForward = true
+		}
+
+		let closestFoodForwardDistance = 1000
+		for (let i = 1; i <= 200; i++) {
+			forwardPositionX = position.x + i * delta.x
+			forwardPositionY = position.y + i * delta.y
+			if (this.getContent(forwardPositionX, forwardPositionY).isObstacle()) {
+				closestFoodForwardDistance += 200 - i
+				break
+			}
+			if (this.getContent(forwardPositionX, forwardPositionY).isFood()) {
+				if (i < closestFoodForwardDistance) {
+					closestFoodForwardDistance = i
+				}
+			}
 		}
 
 		const leftDirection = turnLeft(direction)
 		delta = positionDelta(leftDirection)
-		if (
-			!this.getContent(position.x + delta.x, position.y + delta.y).isObstacle()
-		) {
+		let leftPositionX = position.x + delta.x
+		let leftPositionY = position.y + delta.y
+
+		if (!this.getContent(leftPositionX, leftPositionY).isObstacle()) {
 			canLeft = true
+		}
+
+		let closestFoodLeftDistance = 1000
+		for (let i = 1; i <= 200; i++) {
+			leftPositionX = position.x + i * delta.x
+			leftPositionY = position.y + i * delta.y
+			if (this.getContent(leftPositionX, leftPositionY).isObstacle()) {
+				closestFoodLeftDistance += 200 - i
+				break
+			}
+			if (this.getContent(leftPositionX, leftPositionY).isFood()) {
+				if (i < closestFoodLeftDistance) {
+					closestFoodLeftDistance = i
+				}
+			}
 		}
 
 		const rightDirection = turnRight(direction)
 		delta = positionDelta(rightDirection)
-		if (
-			!this.getContent(position.x + delta.x, position.y + delta.y).isObstacle()
-		) {
+		let rightPositionX = position.x + delta.x
+		let rightPositionY = position.y + delta.y
+		if (!this.getContent(rightPositionX, rightPositionY).isObstacle()) {
 			canRight = true
 		}
 
-		if (canForward && (canLeft || canRight) && Math.random() < 0.98) {
+		let closestFoodRightDistance = 1000
+		for (let i = 1; i <= 200; i++) {
+			rightPositionX = position.x + i * delta.x
+			rightPositionY = position.y + i * delta.y
+			if (this.getContent(rightPositionX, rightPositionY).isObstacle()) {
+				closestFoodRightDistance += 200 - i
+				break
+			}
+			if (this.getContent(rightPositionX, rightPositionY).isFood()) {
+				if (i < closestFoodRightDistance) {
+					closestFoodRightDistance = i
+				}
+			}
+		}
+
+		const smallestFoodDistance = Math.min(
+			Math.min(closestFoodForwardDistance, closestFoodLeftDistance),
+			closestFoodRightDistance
+		)
+		if (smallestFoodDistance >= 1000) {
+			if (smallestFoodDistance == closestFoodForwardDistance) {
+				return
+			} else if (smallestFoodDistance == closestFoodLeftDistance) {
+				this.turnLeft()
+				return
+			} else if (smallestFoodDistance == closestFoodRightDistance) {
+				this.turnRight()
+				return
+			}
+		} else if (closestFoodForwardDistance == smallestFoodDistance) {
+			return
+		} else if (closestFoodLeftDistance == smallestFoodDistance) {
+			this.turnLeft()
+			return
+		} else if (closestFoodRightDistance == smallestFoodDistance) {
+			this.turnRight()
+			return
+		}
+
+		if (canForward) {
 			return
 		}
 
